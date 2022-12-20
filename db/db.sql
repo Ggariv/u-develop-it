bf4d4ed4e59bc0f902bad4a34366efa59ff9cc08
Gonzalo <ggarciarivera09@gmail.com>
2022-12-20 16:18:10 -0500

    update

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,6 @@
 DROP DATABASE IF EXISTS election;
 CREATE DATABASE election;
 USE election;
+
+/* source db/db.sql it will execute this file */
+/* everytime you log in again, you must specify which database use again -> USE <database name>; */
