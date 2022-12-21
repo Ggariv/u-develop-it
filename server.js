5ccc22b3bf116ca7d86621a2f3f536f454f5980c
Gonzalo <ggarciarivera09@gmail.com>
2022-12-21 11:29:38 -0500

    update

--- server.js
+++ server.js
@@ -20,9 +20,35 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
     );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-    });
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+//     });
+
+// To GET a single candidate -> `SELECT * FROM candidates WHERE id = <number> , (err, row) => {}` 
+// db.query(`SELECT * FROM candidates`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//         }
+//     console.log(row);
+//     });
+
+// To DELETE a candidate -> `DELETE FROM candidates WHERE id = ?, 1, (err, result) => {}` 
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//         }
+//     console.log(result);
+//     });
+
+// CREATE a candidate -> `INSERT INTO candidates (column name(s)) VALUES (?s), <param values based on column>, (err, result) => {}`
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+//     });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
